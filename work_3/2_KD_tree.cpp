2f7ee5da44fb6852ea64c59633e8f11a71262954
eumenides <eumescan@qq.com>
2019-01-27 18:57:51 +0800

    work_3 problems

---
+++ work_3/2_KD_tree.cpp
@@ -0,0 +1,31 @@
+/**   2_KD_tree.cpp
+  *    @author:    eumes
+  *    @date:      2019/1/27
+  *    Copyright (c) 2019 eumes. All rights reserved.
+  */
+
+/**
+  *     Title:  KD树构造和查找
+  *     Description:    对给定的点集合构造KD树，要求如下：将方差最大的维度作为当前的分割维度，将数据集在分割维度上
+  *                 排序后的中位数作为分割点。程序要检索给定点对应的最近的K个点的坐标。
+  *     Input:  输入第一行为用例个数，后面为测试用例，每一个用例包含三行，第一行为点集合（点之间用逗号隔开，两个
+  *                 坐标用空格隔开），第二行为检索的点，第三行为K值。
+  *     Output: 输出每一个用例的最近K个点，按照距离从小到大的顺序打印。
+  *     Sample Input:
+  *                 1
+  *                 3 5,6 2,5 8,9 3,8 6,1 1,2 9
+  *                 8.2 4.6
+  *                 2
+  *     Sample Output:
+  *                 8 6,9 3
+  */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
---
+++ work_3/_topo_sort.cpp
@@ -0,0 +1,27 @@
+/**   _topo_sort.cpp
+  *    @author:    eumes
+  *    @date:      2019/1/27
+  *    Copyright (c) 2019 eumes. All rights reserved.
+  */
+
+/**
+  *     Title:  拓扑排序解的个数
+  *     Description:    给定有向无环图中所有边，计算图的拓扑排序解的个数
+  *     Input:  输入第一行为用例个数，后面每一行表示一个图中的所有边，边的起点和终点用空格隔开，边之间使用逗号隔开。
+  *     Output: 输出拓扑排序解的个数
+  *     Sample Input:
+  *                 1
+  *                 a c,b c,c d,d e,d f,e g,f g
+  *     Sample Output:
+  *                 4
+  */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    return 0;
+}
